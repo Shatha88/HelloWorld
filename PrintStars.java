4520402487e313964c8ca046d5301c6cfd48bfba
shatha <shatha.althbiti@gmail.com>
2023-05-02 07:44:19 +0300

    Add Print stars code

---
+++ PrintStars.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+public class PrintStars {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter an odd number:");
+        int rows=sc.nextInt();
+        sc.close();
+        String star="";
+        for(int i=0;i<=rows;i++){
+            if(i>=(rows/2)+1){
+                star = star.substring(0, star.length()-1);
+
+            }else{
+                star+= "*";
+            }
+            System.out.println(star);
+
+        }
+    
+    }
+}
